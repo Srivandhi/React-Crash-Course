fe4b17008576e05989c429c61133281035a57dc8
Srivandhi <srivandhi264@gmail.com>
2025-02-18 21:05:02 +0530

    fundamental concepts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import {Greet} from './components/Greet';  /* component to be used is imported here */
 import { Message } from './components/Message';
-import { Mother } from './components/Mother';
+/* import { Mother } from './components/Mother';
 import { ClickHandler } from './components/ClickHandler';
 import { ConditionalStat } from './components/ConditionStat';
 import { Namelist } from './components/Namelist';
 import { StyleSheet } from './components/StyleSheet';
-import { Inline } from './components/Inline';
+import { Inline } from './components/Inline'; */
 import './appStyles.css';
-import styles from './appStyles.module.css';
+/* import styles from './appStyles.module.css';
 import { Form } from './components/Form';
 import { PostList } from './components/PostList';
+import { PostForm } from './components/PostForm'; */
+
+import NAMES from './components/data.json';
+import { useState,useTransition } from 'react';
 
 
 import './App.css';
 
 function App() {
+
+  const [query,setQuery] = useState('');
+  const [inputValue,setInputValue] = useState('');
+  const [isPending,startTransition] = useTransition(); /* returns two values */
+  const changeHandler = (event)=>{
+    setInputValue(event.target.value);
+    startTransition(()=> setQuery(event.target.value))
+  }
+  const filterNames = NAMES.filter(item=>{
+    return item.firstName.includes(query) || item.lastName.includes(query);
+  })
+
   return (
     <div className="App">
         {/* <Greet name='first' userName='sri' />  we can use this component as many times by simply copying the tag
@@ -33,8 +49,19 @@ function App() {
         <h1 className='error'>Error</h1>
         <h1 className={styles.success}>Success</h1>
         <Inline /> */}
-        <Form />
-        <PostList />
+        {/* <Form />
+        <PostList /> */}
+        {/* <PostForm /> */}
+
+
+          <input type="text" value={inputValue} onChange = {changeHandler}></input>
+          {isPending && <p>Updating list...</p>}
+        {
+          filterNames.map((item)=>(
+            <p key ={item.id}>{item.firstName} {item.lastName}</p>
+            
+          ))
+        }
 
         
     </div>
